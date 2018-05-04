7492c47952a98d543868e92fe9ba7ea1f3472e51
JackMead <Jack.Mead@softwire.com>
2018-05-04 15:07:57 +0100

    Data types and null values

--- FindEmployeesWithoutPension.sql
+++ FindEmployeesWithoutPension.sql
@@ -1,1 +1,3 @@
 SELECT * FROM Employees e WHERE NOT EXISTS (SELECT * FROM PensionFunds WHERE e.Id=EmployeeId)
+
+SELECT * FROM Employees e LEFT OUTER JOIN PensionFunds p ON e.Id=p.EmployeeId WHERE p.EmployeeId IS NULL
--- UpdatePensions.sql
+++ UpdatePensions.sql
@@ -1,2 +1,8 @@
-UPDATE PensionFunds SET Amount=Amount+0.05*Salary
+IF COL_LENGTH('PensionFunds', 'LastContributionDate') IS NULL
+BEGIN
+    ALTER TABLE PensionFunds
+    ADD [LastContributionDate] DATETIME
+END
+
+UPDATE PensionFunds SET Amount=Amount+0.05*Salary, LastContributionDate = GETDATE()
 FROM Employees e JOIN PensionFunds pf on e.Id = pf.EmployeeId;
